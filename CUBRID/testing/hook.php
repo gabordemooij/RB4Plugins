a37b9fe4ce4a5b0eee405022a3b415061dc5ba16
Gabor de Mooij <gabordemooij@gmail.com>
2014-03-21 00:13:35 +0100

    Update hook

--- CUBRID/testing/hook.php
+++ CUBRID/testing/hook.php
@@ -1,11 +1,33 @@
 <?php
+use \R as R;
 
-if ( isset( $ini['CUBRID'] ) ) {
-	$dsn = "cubrid:host={$ini['CUBRID']['host']};port=33000;dbname={$ini['CUBRID']['schema']}";
+//Define a PATH to the plugin as a CONSTANT - for example:
+define('PATH_TO_PLUGIN','../../plugins/CUBRID/'); //put your path to your plugin writer here!
 
-	R::addDatabase( 'CUBRID', $dsn, $ini['CUBRID']['user'], $ini['CUBRID']['pass'], FALSE );
+//Load the CUBRID query writer
+require(PATH_TO_PLUGIN .'CUBRID.php');
+require(PATH_TO_PLUGIN .'testing/CUBRID.php');
 
-	R::selectDatabase( 'CUBRID' );
+//Define a hook path
+$hookPath = PATH_TO_PLUGIN . 'testing/';
 
-	R::exec( 'AUTOCOMMIT IS ON' );
-}
+//Define extra test packages from the hook
+$extraTestsFromHook = array(
+	'CUBRID/Setget',
+	'CUBRID/Writer' 
+);
+
+//Create a connection for this database
+$ini['CUBRID'] = array(
+	'host'	=> 'localhost',
+	'schema' => 'oodb',
+	'user'	=> 'dba',
+	'pass'	=> ''
+);
+
+$colorMap[ 'CUBRID' ] = '0;35';
+
+$dsn = "cubrid:host={$ini['CUBRID']['host']};port=33000;dbname={$ini['CUBRID']['schema']}";
+R::addDatabase( 'CUBRID', $dsn, $ini['CUBRID']['user'], $ini['CUBRID']['pass'], FALSE );
+R::selectDatabase( 'CUBRID' );
+R::exec( 'AUTOCOMMIT IS ON' );
